6d115e9764822421bf68c7924e5026b8b75929ec
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-04-24 04:35:29 -0400

    Working on PUT route for app

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,17 +9,22 @@ module.exports = function (app) {
   });
   app.post("/api/workouts", function (req, res) {
     db.Workout.create(req.body).then((data) => {
-      console.log("Successfully added data to database!");
       res.json(data).end();
     });
   });
   app.put("/api/workouts/:id", function (req, res) {
     var id = req.params.id;
+    var day = Date.now();
     var exercise = [];
     exercise.push(req.body);
-    db.Workout.findByIdAndUpdate(id, exercise, function (err, data) {
-      console.log("Successfully updated document!");
-      res.json(data).end();
+    var update = { day, exercise };
+    db.Workout.findByIdAndUpdate(id, update, { new: true }, function (
+      err,
+      data
+    ) {
+      if (err) throw err;
+      console.log(data);
+      res.json(data);
     });
   });
   app.get("/api/workouts/range", function (req, res) {
